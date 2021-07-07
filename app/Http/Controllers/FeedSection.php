a78e97d55a145d0d571a4cbe55e1dac335adecbf
jovialcore <jollydosky@gmail.com>
2021-07-07 15:54:44 +0100

    expanding the api section so that only needed data for endpoints for fetching data in the feed are  created like id, title, text, created_at

--- app/Http/Controllers/FeedSection.php
+++ app/Http/Controllers/FeedSection.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Posts;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FeedSection extends Controller
 {
     public  function  index()
     {
-        $post =  Posts::get()->toArray();
+        $post =  Posts::get(['id', 'title', 'text', 'created_at'])->toArray();
+        $poster = Posts::first('user_id');
+
+        dd($poster->user_id);
 
         if (isset($post)) {
-            return response()->json(["data" => $post], 200);
+            return response()->json(["data" => $post, 'poster' => 'f' ], 200);
         } else {
             return response()->json([
                 "message" => "No record feed at the moment"
